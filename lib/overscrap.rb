9f78280f4218ae5c7ffaf12cc6f3cd8a666b7f59
Aurelman <just-me@anywhere.com>
2016-08-11 00:22:05 +0200

    Overscrap#user now returns real data

--- lib/overscrap.rb
+++ lib/overscrap.rb
@@ -1,8 +1,10 @@
 require "overscrap/version"
+require 'httparty'
 
 module Overscrap
   # Your code goes here...
-  def self.user(username)
-    "bib"
+  def self.user(username, region=:us, platform=:pc)
+    player = username.gsub('#', '-')
+    HTTParty.get("https://playoverwatch.com/en-us/career/#{platform}/#{region}/#{player}", verify: false)
   end
 end
--- spec/overscrap_spec.rb
+++ spec/overscrap_spec.rb
@@ -5,7 +5,31 @@ RSpec.describe Overscrap do
     expect(Overscrap::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  before :example do
+    @httparty = class_double(HTTParty).as_stubbed_const
+  end
+
+  it 'uses :us as default region and :pc as default platform' do
+    expect(@httparty).to receive(:get).with(/https:\/\/playoverwatch.com\/en-us\/career\/pc\/us\/.*/, any_args)
+
+    Overscrap::user "Bob#31"
+  end
+
+  it 'takes into consideration specified platform and region parameters' do
+    expect(@httparty).to receive(:get).with(/https:\/\/playoverwatch.com\/en-us\/career\/ps4\/eu\/.*/, any_args)
+
+    Overscrap::user "Bob#31", :eu, :ps4
+  end
+
+  it 'takes into consideration region parameters and set platform to :pc by default' do
+    expect(@httparty).to receive(:get).with(/https:\/\/playoverwatch.com\/en-us\/career\/pc\/eu\/.*/, any_args)
+
+    Overscrap::user "Bob#31", :eu
+  end
+
+  it 'transform the given player tag by replacing # char' do
+    expect(@httparty).to receive(:get).with(/https:\/\/playoverwatch.com\/en-us\/career\/pc\/us\/Bob-31/, any_args)
+
+    Overscrap::user "Bob#31"
   end
 end
